78456d2fc45101ad027021bb40e49cdb4d4a8bf8
Jkutkut <45950243+Jkutkut@users.noreply.github.com>
2023-08-15 12:44:23 +0200

    feat: navbar

--- gamecup/src/App.tsx
+++ gamecup/src/App.tsx
@@ -4,6 +4,7 @@ import Game from './model/games/Game';
 import User from './model/User';
 import StorageHandler from './model/db/StorageHandler';
 import GameSelector from './component/gameSelector/GameSelector';
+import Navbar from './component/navbar.tsx/Navbar';
 
 function App() {
   const [gameHandler] = useState<StorageHandler>(StorageHandler.getInstance());
@@ -12,21 +13,26 @@ function App() {
   if (game == null)
     return <GameSelector setGame={setGame} />;
   return <>
-    <h1>{game.getName()}</h1>
-    {game.getTeams().map((team: Team, index) =>
-      <div key={index}>
-        <h4>{team.getName()}</h4>
-        <div className="row">
-          {team.getPlayers().map((user: User, idx) =>
-            <div key={idx} className="col">
-              {user.getName()}
-            </div>
-          )}
+    <Navbar
+      game={game}
+      openGameSelector={() => setGame(null)}
+    />
+    <div className="body-content p-3">
+      {/* <h1 className='p-2'>{game.getName()}</h1> */}
+      {game.getTeams().map((team: Team, index) =>
+        <div key={index}>
+          <h4>{team.getName()}</h4>
+          <div className="row">
+            {team.getPlayers().map((user: User, idx) =>
+              <div key={idx} className="col">
+                {user.getName()}
+              </div>
+            )}
+          </div>
+          <br /><br />
         </div>
-        <br /><br />
-      </div>
-    )}
-    <button type='button' className='btn btn-primary' onClick={() => setGame(null)}>Select game</button>
+      )}
+    </div>
   </>;
 }
 
